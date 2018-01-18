98c39602603ada246e0cff711cb06b70800f3ea7
Milo Carbol <mcarbol@pingidentity.com>
2018-01-17 20:15:06 -0800

    Added types and attributes to DB

--- campaign/admin.py
+++ campaign/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Thing
+from .models import ThingType, Thing, Attribute, AttributeValue
 
 
+admin.site.register(ThingType)
 admin.site.register(Thing)
+admin.site.register(Attribute)
+admin.site.register(AttributeValue)
--- campaign/models.py
+++ campaign/models.py
@@ -1,10 +1,38 @@
 from django.db import models
 
 
-class Thing(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=500)
-    children = models.ManyToManyField('self', blank=True, symmetrical=False)
+class ThingType(models.Model):
+    name = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return self.name
+
+
+class Thing(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=500)
+    children = models.ManyToManyField('self', blank=True, symmetrical=False)
+    thing_type = models.ForeignKey(ThingType, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Attribute(models.Model):
+    thing_type = models.ForeignKey(ThingType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = (('thing_type', 'name'),)
+
+    def __str__(self):
+        return '[' + self.thing_type.name + '] ' + self.name
+
+
+class AttributeValue(models.Model):
+    thing = models.ForeignKey(Thing, on_delete=models.CASCADE)
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '[' + self.thing.name + '] ' + self.attribute.name + ': ' + self.value
