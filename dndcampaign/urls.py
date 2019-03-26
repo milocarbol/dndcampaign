6c9fdf1f0d8dfc588d8e4fe70d04c914a6b23bd7
Milo Carbol <mcarbol@pingidentity.com>
2019-03-26 15:30:44 -0700

    Fixed missing favicon for admin

--- dndcampaign/urls.py
+++ dndcampaign/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('campaign/', include('campaign.urls')),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.jpg', permanent=True))
 ]
